fd9c9941514877076d7adda6b0f94f8eb333d031
marclenotharwaat <marclenotharwat@gmail.com>
2026-04-06 00:33:35 +0200

    add lesson model

---
+++ src/models/lessonModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const lessonShema = mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'A lesson must have a title'],
+    unique: true,
+    trim: true,
+    maxlength: [20, 'A lesson name must have less or equal then 40 characters'],
+    minlength: [3, 'A lesson name must have more or equal then 10 characters'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  videoURL: {
+    type: String,
+    required: [true, 'A lesson must have a videoURL'],
+    trim: true,
+  },
+  course: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Course',
+    required: [true, 'Lesson must belong to a course'],
+  },
+});
+
+const Lesson = mongoose.model('Lesson', lessonShema);
+export default Lesson;
